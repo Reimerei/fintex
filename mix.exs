829a45bbe7a002bdf0594f93c17b5b054944b0f1
Björn Reimer <git@bjrm.de>
2019-02-24 20:42:19 +0100

    Remove runtime warning, update some dependencies

--- mix.exs
+++ mix.exs
@@ -11,15 +11,14 @@ defmodule FinTex.Mixfile do
       elixir: "~> 1.3",
       description: description(),
       package: package(),
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [plt_add_deps: :transitive],
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.travis": :test]
+      preferred_cli_env: [coveralls: :test, "coveralls.travis": :test]
     ]
   end
-
 
   def application do
     [
@@ -35,27 +34,25 @@ defmodule FinTex.Mixfile do
     ]
   end
 
-
   defp deps do
     [
-      {:bankster,              "~> 0.2.2"},
-      {:credo,                 "~> 0.5.3",  only: [:dev, :test]},
-      {:decimal,               "~> 1.3.1"},
-      {:earmark,               "~> 1.0.3",  only: :dev, override: true},
-      {:ex_doc,                "~> 0.14.5", only: :dev},
-      {:exactor,               "~> 2.2.3"},
-      {:excoveralls,           "~> 0.5.7",  only: :test},
-      {:httpotion,             "~> 3.0.2"},
-      {:inch_ex,               "~> 0.5.5",  only: [:dev, :docs]},
-      {:luhnatex,              "~> 0.5.1"},
-      {:mt940,                 "~> 1.1.2"},
-      {:ssl_verify_fun,        "~> 1.1.1"},
-      {:timex,                 "~> 3.1.5"},
-      {:vex,                   "~> 0.6.0"},
-      {:xml_builder,           "~> 0.0.8"}
+      {:bankster, "~> 0.2.2"},
+      {:credo, "~> 0.5.3", only: [:dev, :test]},
+      {:decimal, "~> 1.3"},
+      {:earmark, "~> 1.0.3", only: :dev, override: true},
+      {:ex_doc, "~> 0.14.5", only: :dev},
+      {:exactor, "~> 2.2"},
+      {:excoveralls, "~> 0.5", only: :test},
+      {:httpotion, "~> 3.0"},
+      {:inch_ex, "~> 0.5.5", only: [:dev, :docs]},
+      {:luhnatex, "~> 0.5.1"},
+      {:mt940, "~> 1.1.2"},
+      {:ssl_verify_fun, "~> 1.1.1"},
+      {:timex, "~> 3.1", override: true},
+      {:vex, "~> 0.6"},
+      {:xml_builder, "~> 0.0.8"}
     ]
   end
-
 
   defp description do
     """
@@ -63,13 +60,12 @@ defmodule FinTex.Mixfile do
     """
   end
 
-
   defp package do
     [
-      files:       ["lib", "priv", "mix.exs", "README*", "LICENSE*",],
+      files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
       maintainers: ["Florian J. Breunig"],
-      licenses:    ["MIT"],
-      links:       %{"GitHub" => "https://github.com/my-flow/fintex"}
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/my-flow/fintex"}
     ]
   end
 end
